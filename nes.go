edbcd2394dd9955d99f24cab3ea639b1c60e4be0
Jose Pulgar <webmaster@josepulgar.com>
2021-07-10 23:05:01 -0500

    NES support

--- nes.go
+++ nes.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"strconv"
+	"html/template"
 	"strings"
 )
 
 // var arcadeSets []string
 //var nesGameInfo = make(map[string]NESEntry)
+
+var nesnames = make(map[string]bool)
+var nesimages = make(map[string]string)
+var uniquenesnames []string
 
 // JSON Structure
 // type NESEntry struct {
@@ -15,32 +20,36 @@ import (
 // 	Image string `json:"image"`
 // }
 
-// // HTML Structure
-// type IndexPageData struct {
-// 	Alphabet1 []string
-// 	Alphabet2 []string
-// 	Alphabet3 []string
-// 	Years     []string
-// }
+type NESListPageData struct {
+	Sections    []Section
+	CurrentPage string
+	Games       []Game
+}
 
-// type ArcadeGamePageData struct {
-// 	ID     string
-// 	Name   string
-// 	Year   string
-// 	Author string
-// 	Moves  template.HTML
-// 	Video  string
-// }
+type Game struct {
+	Page  string
+	Image string
+	Name  string
+}
+
+type Section struct {
+	Name  string
+	Label string
+}
+
+type NESGamePageData struct {
+	Name  string
+	Image string
+	Video string
+}
 
 func generateMisterNESGames() {
 	generateMisterNESNamesJSON()
+	generateMisterNESHTML()
+	CopyNESScripts()
 }
 
 func generateMisterNESNamesJSON() {
-	//nesentries := []NESEntry{}
-	nesnames := make(map[string]bool)
-	nesimages := make(map[string]string)
-	var uniquenesnames []string
 
 	for _, f := range findAllFiles("assets/nes/titles", ".png", "") {
 
@@ -61,8 +70,6 @@ func generateMisterNESNamesJSON() {
 			nesnames[f] = true
 			uniquenesnames = append(uniquenesnames, f)
 			nesimages[f] = original
-			// nesentry := NESEntry{Name: f, Image: original}
-			// nesentries = append(nesentries, nesentry)
 		} else {
 			if strings.Contains(original, "USA") {
 				nesimages[f] = original
@@ -70,33 +77,38 @@ func generateMisterNESNamesJSON() {
 		}
 	}
 
-	// for _, v := range uniquenesnames {
-	//fmt.Println(v + " --> " + nesimages[v]) // Show All
+	for _, v := range uniquenesnames {
 
-	// if strings.Contains(nesimages[v], "USA") { // Show USA released games
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Unl)") { // Show Unlicensed only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Australia)") { // Show Australia only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Asia)") { // Show Asia only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Korea)") { // Show Korea only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Europe)") { // Show Europe only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	// if strings.Contains(nesimages[v], "(Japan)") { // Show Europe only
-	// 	fmt.Println(v + " --> " + nesimages[v])
-	// }
-	//}
+		if false {
+			// fmt.Println(v + " --> " + nesimages[v]) // Show All
+			fmt.Println(urlSafe(v)) // Show All
+		}
 
-	fmt.Println("Unique NES games: " + strconv.Itoa(len(uniquenesnames)))
+		// if strings.Contains(nesimages[v], "USA") { // Show USA released games
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Unl)") { // Show Unlicensed only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Australia)") { // Show Australia only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Asia)") { // Show Asia only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Korea)") { // Show Korea only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Europe)") { // Show Europe only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+		// if strings.Contains(nesimages[v], "(Japan)") { // Show Europe only
+		// 	fmt.Println(v + " --> " + nesimages[v])
+		// }
+	}
+
+	//fmt.Println("Unique NES games: " + strconv.Itoa(len(uniquenesnames)))
+
 	// Sort entries
 	// sort.Slice(entries, func(i, j int) bool {
 	// 	return entries[i].Name < entries[j].Name
@@ -109,4 +121,84 @@ func generateMisterNESNamesJSON() {
 	// }
 	// WriteToFile("mister/arcade/name.json", string(prettyJSON))
 	// WriteToFile("public/mister/arcade/name.json", string(prettyJSON))
+}
+
+func generateMisterNESHTML() {
+
+	var tmplBuffer bytes.Buffer
+
+	// Generate NES Games
+	if false {
+		for _, v := range uniquenesnames {
+
+			nesVideo := nesVideos[v]
+
+			dataGames := NESGamePageData{
+				Name:  v,
+				Image: nesimages[v],
+				Video: nesVideo,
+			}
+			tmpl := template.Must(template.ParseFiles("mister/nes/game_layout.html", "navigation.html"))
+			if err := tmpl.Execute(&tmplBuffer, dataGames); err != nil {
+				fmt.Println(err)
+			}
+			WriteToFile("public/mister/nes/games/"+urlSafe(v)+".html", tmplBuffer.String())
+			tmplBuffer.Reset()
+
+		}
+	}
+
+	// Generate num.html, a.html, b.html, c.html, ..., textlist.html
+	listFilename := [28]string{"num", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+		"q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "textlist"}
+	for _, v := range listFilename {
+
+		var tempGames []Game
+		for _, g := range uniquenesnames {
+			temp := Game{urlSafe(g), nesimages[g], g}
+			tempGames = append(tempGames, temp)
+		}
+
+		data := NESListPageData{
+			Sections: []Section{{"num", "#"}, {"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}, {"e", "E"}, {"f", "F"},
+				{"h", "H"}, {"i", "I"}, {"j", "J"}, {"k", "K"}, {"l", "L"}, {"m", "M"}, {"n", "N"}, {"o", "O"}, {"p", "P"},
+				{"q", "Q"}, {"r", "R"}, {"s", "S"}, {"t", "T"}, {"u", "U"}, {"v", "V"}, {"w", "W"}, {"x", "X"}, {"y", "Y"},
+				{"z", "Z"}, {"textlist", "Text List"}},
+			CurrentPage: v,
+			Games:       tempGames,
+		}
+
+		tmpl := template.Must(template.ParseFiles("mister/nes/list_layout.html", "navigation.html"))
+		if err := tmpl.Execute(&tmplBuffer, data); err != nil {
+			fmt.Println(err)
+		}
+		WriteToFile("public/mister/nes/"+strings.ToLower(v)+".html", tmplBuffer.String())
+		tmplBuffer.Reset()
+	}
+}
+
+func urlSafe(name string) string {
+	name = strings.Replace(name, " - ", "-", -1)
+	name = strings.Replace(name, " ", "-", -1)
+	name = strings.Replace(name, "(", "", -1)
+	name = strings.Replace(name, ")", "", -1)
+	name = strings.Replace(name, ",", "", -1)
+	name = strings.Replace(name, "'", "", -1)
+	name = strings.Replace(name, ".", "", -1)
+	name = strings.Replace(name, "&", "and", -1)
+	name = strings.Replace(name, "!", "", -1)
+	name = strings.Replace(name, "?", "", -1)
+	name = strings.Replace(name, "$", "", -1)
+	name = strings.Replace(name, "+", "and", -1)
+	name = strings.Replace(name, "[", "", -1)
+	name = strings.Replace(name, "]", "", -1)
+	return name
+}
+
+func CopyNESScripts() {
+	err := CopyFile("mister/nes/nes.css", "public/mister/nes/nes.css")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
